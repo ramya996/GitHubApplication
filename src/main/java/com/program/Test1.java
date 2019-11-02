b629a402ea2c8ea07f099f3e9135204494ba75e2
Ramya <ramya>
2019-11-02 14:18:32 +0530

    First Commit......

---
+++ src/main/java/com/program/Test1.java
@@ -0,0 +1,22 @@
+package com.program;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		String str="yes";
+		
+		if(str.equals("yes") || str.equals("y") || str.equals("Y"))
+		{
+				
+			System.out.println("yesss");	
+		}
+		
+		else
+		{
+			System.out.println("this is wrong");	
+		}
+		
+
+	}
+
+}
